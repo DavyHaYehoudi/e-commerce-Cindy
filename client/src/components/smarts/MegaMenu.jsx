39c0b6dabc3e05fbed897fafede14fca05db8af4
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-10-05 19:00:40 +0200

    restructuration menu avec MegaMenuItems + correction superposition des deux menus + regex url pour les routes

--- client/src/components/smarts/MegaMenu.jsx
+++ client/src/components/smarts/MegaMenu.jsx
@@ -1,160 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import bo from "../../assets/bo.png";
+import MegaMenuItem from "./MegaMenuItem";
 
 const MegaMenu = ({ megaMenuSection }) => {
   const isDisplay1Visible = megaMenuSection === "display1";
   const isDisplay2Visible = megaMenuSection === "display2";
+
   return (
     <div id="mega-Menu">
-      <div
-        className={`mega-Menu-Display ${
-          isDisplay1Visible || isDisplay2Visible ? "visible" : "hidden"
-        }`}
-      >
-        <div
-          className={`mega-Menu-Display-1 ${!isDisplay1Visible && "hidden"}`}
-        >
-          <div>
-            <h3>
-              <Link to="menu-tab-category/accessoires-cheveux">
-                Accessoires cheveux{" "}
-              </Link>{" "}
-            </h3>
-            <div>
-              <ul>
-                <li>
-                  <Link to="menu-tab-subcategory/couronnes">Couronnes</Link>{" "}
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/peignes-et-pics">
-                    Peignes et pics
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <h3>
-              <Link to="menu-tab-category/bijoux">Bijoux</Link>
-            </h3>
-            <div>
-              <ul>
-                <li>
-                  <Link to="menu-tab-subcategory/boucles-d-oreilles">
-                    Boucles d'oreilles
-                  </Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/bracelets">Bracelets</Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/colliers">Colliers</Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/colliers-de-dos">
-                    Colliers de dos
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <h3>
-              <Link to="menu-tab-category/demoiselles-d-honneur">
-                Demoiselles d'honneur
-              </Link>
-            </h3>
-            <div>
-              <ul>
-                <li>
-                  <Link to="menu-tab-subcategory/bracelets">Bracelets</Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/cartes-cadeau">
-                    Cartes cadeau
-                  </Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/coffret-cadeau">
-                    Coffret cadeau
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+      <div className={`mega-Menu-Display ${isDisplay1Visible || isDisplay2Visible ? "visible" : "hidden"}`}>
+        <div className={`mega-Menu-Display-1 ${isDisplay1Visible ? "visible" : "hidden"}`}>
+          <MegaMenuItem
+            category="Accessoires Cheveux"
+            subcategories={["Couronnes", "Peignes et Pics"]}
+          />
+          <MegaMenuItem
+            category="Bijoux"
+            subcategories={["Boucles d'oreilles", "Bracelets", "Colliers", "Colliers de dos"]}
+          />
+          <MegaMenuItem
+            category="Demoiselles d'honneur"
+            subcategories={["Bracelets", "Cartes cadeau", "Coffret cadeau"]}
+          />
           <figure>
             <img src={bo} alt="" />
             <figcaption>Mes boucles d'oreilles</figcaption>
           </figure>
         </div>
-        <div
-          className={`mega-Menu-Display-2 ${!isDisplay2Visible && "hidden"}`}
-        >
-          <div>
-            <h3>
-              <Link to="menu-tab-category/accessoires-cheveux">
-                Accessoires cheveux
-              </Link>
-            </h3>
-            <div>
-              <ul>
-                <li>
-                  <Link to="menu-tab-subcategory/chouchous">Chouchous</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <h3>
-              <Link to="menu-tab-category/bien-etre">Bien-être</Link>
-            </h3>
-            <div>
-              <ul>
-                <li>
-                  <Link to="menu-tab-subcategory/eaux-florales">
-                    Eaux florales
-                  </Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/pochettes-maquillage">
-                    Pochettes maquillage
-                  </Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/roll-on-visage">
-                    Roll-on visage
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <h3>
-              <Link to="menu-tab-category/bijoux">bijoux</Link>
-            </h3>
-            <div>
-              <ul>
-                <li>
-                  <Link to="menu-tab-subcategory/bijoux-de-mains">
-                    Bijoux de mains
-                  </Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/boucles-d-oreilles">
-                    Boucles d'oreilles
-                  </Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/bracelets">Bracelets</Link>
-                </li>
-                <li>
-                  <Link to="menu-tab-subcategory/colliers">Colliers</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-
+        <div className={`mega-Menu-Display-2 ${isDisplay2Visible ? "visible" : "hidden"}`}>
+          <MegaMenuItem
+            category="Accessoires Cheveux"
+            subcategories={["Chouchous"]}
+          />
+          <MegaMenuItem
+            category="Bien-être"
+            subcategories={["Eaux florales", "Pochettes maquillage", "Roll-on visage"]}
+          />
+          <MegaMenuItem
+            category="Bijoux"
+            subcategories={["Bijoux de mains", "Boucles d'oreilles", "Bracelets", "Colliers"]}
+          />
           <figure>
             <img src={bo} alt="" />
             <figcaption>Mes boucles d'oreilles</figcaption>
